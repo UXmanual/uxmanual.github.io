0777af2602dd99e0cf650051b4bee2f963ef657b
Rich <rich@uxmanual.com>
2026-02-19 13:13:28 +0900

    포트폴리오 페이지를 메인(루트) 페이지로 변경 및 이전 메인 삭제

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 import Portfolio from '../views/Portfolio.vue'
 
 const routes = [
-    { path: '/', component: Home },
-    { path: '/portfolio', component: Portfolio },
+    { path: '/', component: Portfolio },
 ]
 
 const router = createRouter({
