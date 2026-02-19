c186d403934ae1f9594666b64d7a227351f85fe0
Rich <rich@uxmanual.com>
2026-02-19 13:15:18 +0900

    구버전 경로(#/portfolio) 삭제 및 자동 리다이렉트 설정

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Portfolio from '../views/Portfolio.vue'
 
 const routes = [
     { path: '/', component: Portfolio },
+    { path: '/portfolio', redirect: '/' },
+    { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
 
 const router = createRouter({
