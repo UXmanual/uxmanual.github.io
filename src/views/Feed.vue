bee20173484f9558e002d83f157b2f0d8957f700
UXmanual <139927@naver.com>
2026-02-24 20:30:39 +0900

    docs: update feed page header text

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -9,7 +9,7 @@
 
     <SiteHeader 
       title="Feed" 
-      description="뉴스보다 더 재밌는 우리들의 이야기 <br> 함께 읽고, 함께 말해요"
+      description="뉴스보다 더 재밌는 우리들의 이야기 <br> 함께 읽고, 함께 말해요!"
       padding-top="pt-16"
       margin-bottom="mb-16"
       inner-max-width="max-w-[680px] mx-auto"
