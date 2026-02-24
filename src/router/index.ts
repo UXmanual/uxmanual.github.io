0e585dfcd418588e7e652fc2dea8fe5d44e4115f
Rich <rich@uxmanual.com>
2026-02-24 10:41:49 +0900

    refactor: rename community to feed and implement ultimate URL sanitizer

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Community from '../views/Community.vue'
+import Feed from '../views/Feed.vue'
 import Trends from '../views/Trends.vue'
 
 const routes = [
     { path: '/', component: Trends },
-    { path: '/feed', component: Community },
-    { path: '/community', redirect: '/feed' },
+    { path: '/feed', component: Feed },
     { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
 
