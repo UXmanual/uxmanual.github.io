39566b594892ad09168b52924c2247e04d494fd8
Rich <rich@uxmanual.com>
2026-02-24 10:36:05 +0900

    simplify(routing): switch to hash history and remove complex 404 redirects

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Community from '../views/Community.vue'
 import Trends from '../views/Trends.vue'
 
 const routes = [
     { path: '/', component: Trends },
     { path: '/feed', component: Community },
-    { path: '/trends', redirect: '/' },
-    { path: '/community', redirect: '/feed' },
     { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 })
 
