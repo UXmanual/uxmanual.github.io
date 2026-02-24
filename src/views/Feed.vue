e8096110df61d2fe8fb005fecc39c81f3fb39a56
Rich <rich@uxmanual.com>
2026-02-24 17:27:07 +0900

    feat(feed): include time in post date

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -169,7 +169,7 @@ const addPost = async () => {
   isPosting.value = true
   
   const now = new Date()
-  const dateStr = `${now.getFullYear()}.${(now.getMonth() + 1).toString().padStart(2, '0')}.${now.getDate().toString().padStart(2, '0')}`
+  const dateStr = `${now.getFullYear()}.${(now.getMonth() + 1).toString().padStart(2, '0')}.${now.getDate().toString().padStart(2, '0')} ${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}`
   
   const newPost = {
     name: newName.value,
