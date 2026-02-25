7de5fb3e9a72fd712441efa21cb6c30e6e0e55a0
Rich <rich@uxmanual.com>
2026-02-25 10:19:59 +0900

    content: update banner message

--- src/components/SiteBanner.vue
+++ src/components/SiteBanner.vue
@@ -2,7 +2,7 @@
   <div class="bg-zinc-100 dark:bg-zinc-900 px-6 py-4 overflow-hidden relative">
     <div class="max-w-[1800px] mx-auto flex items-center justify-center text-center">
       <p class="text-[13px] font-medium text-zinc-500 dark:text-zinc-400 tracking-tight leading-none">
-        Todays News의 새로운 피드 기능을 만나보세요
+        더 똑똑해지는 중... 🤖
       </p>
     </div>
   </div>
