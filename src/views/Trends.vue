fc7867d8cde746269d338bdf33146c782844a263
Rich <rich@uxmanual.com>
2026-03-03 10:51:30 +0900

    Refactor: Switch Diablo2 sources to direct Korean community feeds, Version Bump to 12.4

--- src/views/Trends.vue
+++ src/views/Trends.vue
@@ -179,7 +179,7 @@ import SiteFooter from '../components/SiteFooter.vue'
 import SiteHeader from '../components/SiteHeader.vue'
 import SiteBanner from '../components/SiteBanner.vue'
 
-const CURRENT_CACHE_VERSION = 'v12.3'
+const CURRENT_CACHE_VERSION = 'v12.4'
 const CACHE_KEY = `uxm_trends_cache_${CURRENT_CACHE_VERSION}`
 
 interface NewsItem {
@@ -426,11 +426,11 @@ const RSS_SOURCES = [
   { name: '플래텀 (Insurtech)', url: 'https://platum.kr/feed', category: 'goodrich' },
   { name: '보험신보', url: 'http://www.insweek.co.kr/rss/all.xml', category: 'goodrich' },
 
-  // Diablo2 (News & Community Issues)
-  { name: '인벤 디아2', url: 'https://www.inven.co.kr/common/rss/rss.xml?id=diablo2', category: 'diablo2' },
-  { name: '루리웹 디아2', url: 'https://rss.ruliweb.com/rss/pc_diablo2.xml', category: 'diablo2' },
-  { name: '디아블로2 이슈 (Google)', url: 'https://news.google.com/rss/search?q=디아블로2+레저렉션+when:1d&hl=ko&gl=KR&ceid=KR:ko', category: 'diablo2' },
-  { name: 'Diablo 2 Global (Korean)', url: 'https://news.google.com/rss/search?q=Diablo+2+Resurrected+when:1d&hl=ko&gl=KR&ceid=KR:ko', category: 'diablo2' }
+  // Diablo2 (Direct Community & Game News Feeds)
+  { name: '인벤 디아2 (커뮤니티)', url: 'https://www.inven.co.kr/common/rss/rss.xml?id=diablo2', category: 'diablo2' },
+  { name: '루리웹 디아2 (커뮤니티)', url: 'https://rss.ruliweb.com/rss/pc_diablo2.xml', category: 'diablo2' },
+  { name: '루리웹 PC 게임 뉴스', url: 'https://rss.ruliweb.com/rss/pc_news.xml', category: 'diablo2' },
+  { name: '인벤 IT/게임 뉴스', url: 'https://www.inven.co.kr/common/rss/rss.xml?id=it', category: 'diablo2' }
 ]
 
 const filteredNews = computed(() => {
