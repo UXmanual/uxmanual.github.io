2f7000b7d8dc089c8fd414f053a30cf718c0bd4a
Rich <rich@uxmanual.com>
2026-03-04 16:04:09 +0900

    style: tone up SiteBanner in dark mode for better distinction from background

--- src/components/SiteBanner.vue
+++ src/components/SiteBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-zinc-100 dark:bg-zinc-900 px-6 py-4 overflow-hidden relative">
+  <div class="bg-zinc-100 dark:bg-[#1f1f1f] px-6 py-4 overflow-hidden relative">
     <div class="max-w-[1800px] mx-auto flex items-center justify-center text-center">
       <p class="text-[13px] font-medium text-zinc-500 dark:text-zinc-400 tracking-tight leading-none">
         데이터 정보가 많은 경우 시간이 걸릴 수 있습니다.. ⏳
