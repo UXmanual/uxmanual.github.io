d12d3f9380f105a4924975358b475f8af2a4f55d
Rich <rich@uxmanual.com>
2026-03-11 10:38:27 +0900

    fix: corrects naver maps sdk parameter casing and bump to v48.7

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -25,7 +25,7 @@
           © NUXA
         </p>
         <div class="text-[12px] font-bold text-zinc-300 dark:text-[#ddd]">
-          ver. 48.6
+          ver. 48.7
         </div>
       </div>
     </div>
